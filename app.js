81d285be2bf4ec9d1f8a59c00637a6750c312d42
serverless-admin <i4itechnical@gmail.com>
2019-08-15 20:36:32 +0530

    db

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ const app = express();
 
 // db
 mongoose
-    .connect(process.env.MONGO_URI, {
+    .connect(process.env.DATABASE, {
         useNewUrlParser: true,
         useCreateIndex: true
     })
